7e45b7d3c08993274407fe06efbf768420691dbc
J. Ryan Stinnett <jryans@gmail.com>
2015-11-23 17:57:27 -0600

    Start sketching ws server

---
+++ bin/valence.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const WebSocketServer = require("ws").Server;
+const valenceServer = require("../lib/chromium/server");
+const Task = require("../lib/util/task");
+
+let wsServer = new WebSocketServer({ port: 9000 });
+
+wsServer.on("connection", connection => {
+  connection.on("message", console.log);
+  connection.send("hello");
+
+  Task.spawn(function*() {
+    let url = "http://localhost:9222";
+    try {
+      yield valenceServer.ping(url);
+    } catch (e) {
+      return Promise.reject(new Error("Chrome Desktop not found"));
+    }
+    let transport = valenceServer.connect(url);
+    connection.connect(transport);
+  });
+});
